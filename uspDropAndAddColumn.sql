6448248a414a15129011c95c129ffccbd31fd09d
mapgiedev <4250209+mapgie@users.noreply.github.com>
2021-04-27 17:13:47 +0100

    Check if col exists before count distinct

--- uspDropAndAddColumn.sql
+++ uspDropAndAddColumn.sql
@@ -1,19 +1,37 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Scalar Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECTPROPERTY (object_id('dbo.uspDropAndAddColumn'), 'IsProcedure') = 1
+DROP PROCEDURE [dbo].[uspDropAndAddColumn]
+GO
+
 -- =============================================
--- Author:    	mapgie
+-- Author:		ms16865
 -- Create date: 27/04/2021
 -- Description: Drops a column and re-adds column IF:
 				-- Column only contains nulls 
 				-- @safemode is OFF ( = 0 )		-- @Safemode is always 1 by default 
 -- =============================================
-
 CREATE PROCEDURE uspDropAndAddColumn 
 (
 	@objectname	varchar(50),
 	@newcolname	varchar(50),
 	@datatype	varchar(50),
-	@checkWithSelect bit = 0,	  -- 1 - runs an extra select to check 
-	@debug bit = 0,             -- 1 - prints extra messages 
-	@safemode bit = 1	          -- 1 - default is ON
+	@checkWithSelect bit = 0,	 -- 1 - runs an extra select to check 
+	@debug bit = 0,				 -- 1 - prints extra messages 
+	@safemode bit = 1			 -- 1 - default is ON
 )
 AS
 BEGIN
@@ -23,6 +41,7 @@ BEGIN
 
 	DECLARE @dropquery nvarchar(max), @addquery nvarchar(max), @selectquery nvarchar(max);
 	DECLARE @returnval int, @ParmDefinition nvarchar(500);
+	PRINT @objectName
 
 	SET @dropquery	= 'ALTER TABLE ' + @objectname + ' DROP COLUMN ' + @newcolname;
 	SET @addquery	= REPLACE(@dropquery,'DROP COLUMN','ADD') + ' ' + @datatype;
@@ -35,19 +54,27 @@ BEGIN
 
 	/*DO NOT DROP COLUMNS THAT ARE NOT EMPTY */
 	BEGIN /* CHECK COL EMPTY*/
-	EXEC sp_executesql @selectquery, @ParmDefinition, @returnvalOUT=@returnval OUTPUT;
 
-	IF @returnval = 0 
-	BEGIN
-		SET @status = 1
-		print 'Column contains only null values - can be removed'
-	END
-	ELSE 
-	BEGIN
-		SET @status = 0
-		Print 'Procedure cannot be used to drop columns that contain values other than NULL'
-		GOTO EOF
+	IF EXISTS (SELECT * FROM sys.columns WHERE [object_id] = OBJECT_ID(@objectName) AND [name] = @newcolname)
+	BEGIN 
+
+		EXEC sp_executesql @selectquery, @ParmDefinition, @returnvalOUT=@returnval OUTPUT;
+		IF @returnval = 0 
+		BEGIN
+			SET @status = 1
+			print 'Column contains only null values - can be removed'
+		END
+		ELSE 
+		BEGIN
+			SET @status = 0
+			Print 'Procedure cannot be used to drop columns that contain values other than NULL'
+			GOTO EOF
+		END 
+
 	END 
+	ELSE
+		SET @status = 1
+
 
 	END /* EMPTY CHECK */ 
 	
@@ -61,7 +88,7 @@ BEGIN
 		print 'Successful Column Drop'
 	END		/* COL DROP */
 	
-	IF @status =  1
+	IF @status = 1 
 	BEGIN /* COL ADD */
 		if @debug = 1 print @addquery
 		if @safemode != 1 EXEC sp_sqlexec @addquery
@@ -82,8 +109,10 @@ BEGIN
 	EOF:
 
 	IF @status = 1
-		Print 'End of File'
+		Print 'End of File
+		'
 	ELSE
-		Print 'Error'
+		Print 'Error on ' + cast(@objectName as char) + '
+		'
 
 END
